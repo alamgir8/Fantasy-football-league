5ce75a94107205ebc2a3d9030ff7fd82dd1b06e3
alamgir8 <ahossain38900@gmail.com>
2021-03-10 11:56:17 +0600

    updated

--- src/Components/TeamDetails/TeamDetails.js
+++ src/Components/TeamDetails/TeamDetails.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import twitter from '../../img/Twitter.png';
 import facebook from '../../img/Facebook.png';
-import youtube from '../../img/YouTube.png'
+import youtube from '../../img/YouTube.png';
+import instagram from '../../img/instagram.png';
+import male from '../../img/male.jpg';
+import female from '../../img/female.png'
 
 
 const TeamDetails = (props) => {
-    const {intFormedYear, strCountry, strTeamBadge, strTeam, strSport, strGender, strDescriptionEN, strStadiumDescription, strTwitter, strFacebook, strYoutube}= props.team;
+    const {intFormedYear, strCountry, strTeamBadge, strTeam, strSport, strGender, strDescriptionEN, strStadiumDescription, strInstagram, strTwitter, strFacebook, strYoutube}= props.team;
+
+
+    let playerType;
+    if (strGender === "Male") {
+        playerType = <img src={male} alt=""/>
+    }
+    else{
+        playerType = <img src={female} alt=""/>
+    }
+
     return (
         <div>
-            <h3>This is Team Details</h3>
+           
+            <div>
             <img src={strTeamBadge} alt=""/>
+            </div>
+            <div>
             <h2>{strTeam}</h2>
             <p>Founded : <strong>{intFormedYear}</strong></p>
             <p>Country : <strong>{strCountry}</strong></p>
-            <p>Sport Type :<strong>{strSport}</strong></p>
+            <p>Sport Type : <strong>{strSport}</strong></p>
             <p>Gender : {strGender}</p>
+            <p>{playerType}</p>
+            </div>
             <p>{strDescriptionEN}</p>
             <p>{strStadiumDescription}</p>
+            <a href={strInstagram}> <img src={instagram} alt=""/></a>
             <a href={strTwitter} target='_blank'><img src={twitter} alt=""/></a>
             <Link to={`/team/${strFacebook}`}><img src={facebook} alt=""/></Link>
             <Link to={`/team/${strYoutube}`}><img src={youtube} alt=""/></Link> 
