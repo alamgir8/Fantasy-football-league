46a7e2d96d63d7efda77c25b6cad28b835baec68
alamgir8 <ahossain38900@gmail.com>
2021-03-11 12:20:10 +0600

    final updated

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap';
 
 const NoMatch = () => {
+ 
     return (
-        <div>
-            <h3>404 error !!! page not found !!</h3>
+        <div className='container mt-5'>
+            
+                {[ 'danger' ].map((variant, idx) => 
+                    (<Alert key={idx} variant={variant}>
+                        <h3 className='text-center p-3'>This is a 404 error !!! page not found !! &#128533; </h3>
+                        <Alert.Link href={`https://en.wikipedia.org/wiki/HTTP_404`}><h4 className='text-center p-3'>Know more about it...&#128578;</h4></Alert.Link>.
+                    </Alert> ))} 
         </div>
+       
+   
     );
 };
 
